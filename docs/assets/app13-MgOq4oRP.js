6934848d64eb9922f9820a9c46026222bc369e93
mpopel <mikhail.popel@gmail.com>
2024-07-19 20:35:13 +0300

    remove docs for now

--- docs/assets/app13-MgOq4oRP.js
+++
@@ -1,1 +0,0 @@
-import"vue";import"vue-router";import"@browser-module/stores/counter";import"./--plugin-vue--export-helper-DlAUqK2U.js";import{default as e}from"./admin-6ayXDyX-.js";export{e as default};
