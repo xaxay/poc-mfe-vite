673df1b21d19637227909cfaca516523f2cda7e8
mpopel <mikhail.popel@gmail.com>
2024-07-22 16:13:55 +0300

    update build files

--- docs/assets/app13-2uoBC4sP.js
+++
@@ -1,1 +0,0 @@
-import"vue";import"vue-router";import"@browser-module/stores/counter";import"./--plugin-vue--export-helper-DlAUqK2U.js";import{default as e}from"./admin-CY1-2B4H.js";export{e as default};
