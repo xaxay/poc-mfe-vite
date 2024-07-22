580a17257d26579456cb6bf4892311f5a9bd3d3f
mpopel <mikhail.popel@gmail.com>
2024-07-22 17:03:46 +0300

    make Dashboard.vue using TS

--- src/pages/Dashboard.vue
+++ src/pages/Dashboard.vue
@@ -1,71 +1,71 @@
 // components/Dashboard.vue
 
-<template><v-container fluid>
-  <v-text-field v-model="searchQuery" label="Search Apps" flat clearable
-    prepend-inner-icon="mdi-magnify"></v-text-field>
-  <div class="app-grid">
-    <v-hover v-for="(app, appIndex) in filteredApps" :key="app.name" v-slot:default="{ isHovering, props }">
-      <v-card @click="openApp(app.route)" class="app-card" :elevation="isHovering ? 16 : 2" v-bind="props"
-        :color="appIndex % 2 === 0 ? 'green' : 'blue'">
-        <v-card-text class="text-center">
-          <div class="icon-circle">
-            <v-icon class="app-icon" size="5rem">{{ app.icon }}</v-icon>
-          </div>
-          <div class="app-name">{{ app.name }}</div>
-        </v-card-text>
-        <v-tooltip activator="parent" location="top" offset="-10">Click to open</v-tooltip>
-      </v-card>
-    </v-hover>
-  </div>
-</v-container></template>
+<template>
+  <v-container fluid>
+    <v-text-field v-model="searchQuery" label="Search Apps" flat clearable prepend-inner-icon="mdi-magnify"></v-text-field>
+    <div class="app-grid">
+      <v-hover v-for="(app, appIndex) in filteredApps" :key="app.name" v-slot:default="{ isHovering, props }">
+        <v-card @click="openApp(app.route)" class="app-card" :elevation="isHovering ? 16 : 2" v-bind="props" :color="appIndex % 2 === 0 ? 'green' : 'blue'">
+          <v-card-text class="text-center">
+            <div class="icon-circle">
+              <v-icon class="app-icon" size="5rem">{{ app.icon }}</v-icon>
+            </div>
+            <div class="app-name">{{ app.name }}</div>
+          </v-card-text>
+          <v-tooltip activator="parent" location="top" offset="-10">Click to open</v-tooltip>
+        </v-card>
+      </v-hover>
+    </div>
+  </v-container>
+</template>
 
-<script>
-  import { ref, computed } from 'vue';
-  import { router } from '@browser-module/router';
-  import routesConfig from '@browser-module/config/routes';
-  import { getUserPermissions } from '@browser-module/api/user';
+<script lang="ts">
+import { ref, computed, Ref } from 'vue';
+import { router } from '@browser-module/router';
+import routesConfig, { RouteDef } from '@browser-module/config/routes';
+import { getUserPermissions } from '@browser-module/api/user';
 
-  export default {
-    setup() {
-      const apps = ref([]);
-      const searchQuery = ref('');
-  
-      const routeEntries = Object.entries(routesConfig.routes);
 
-      const permissions = getUserPermissions();
+export default {
+  setup() {
+    const apps : Ref<RouteDef[]> = ref<RouteDef[]>([]);
+    const searchQuery = ref<string>('');
 
-      function isAllowedApp(appName) {
-        const allowed = permissions.includes(appName);
-        return allowed;
-      }
+    const routeEntries: [string, RouteDef][] = Object.entries(routesConfig.routes);
 
-      routeEntries
-        .filter(([path, data]) => (data.dashboard === true) && isAllowedApp(path.substring(1)))
-        .forEach(([path, data]) => {
-          apps.value.push({
-            name: data.title,
-            icon: data.icon || 'mdi-application',
-            route: path
-          });
+    const permissions = getUserPermissions();
+
+    function isAllowedApp(appName: string): boolean {
+      return permissions.includes(appName);
+    }
+
+    routeEntries
+      .filter(([path, data]) => (data.dashboard === true) && isAllowedApp(path.substring(1)))
+      .forEach(([path, data]) => {
+        apps.value.push({
+          name: data.title,
+          icon: data.icon || 'mdi-application',
+          route: path
         });
-  
-      const openApp = (route) => {
-        router.push(route);
-      };
-  
-      const filteredApps = computed(() => {
-        const search = searchQuery.value.toLowerCase();
-        return apps.value.filter(app => !searchQuery.value || app.name.toLowerCase().includes(search));
       });
 
-      return {
-        searchQuery,
-        filteredApps,
-        openApp
-      };
-    }
-  };
-  </script>
+    const openApp = (route: string) => {
+      router.push(route);
+    };
+
+    const filteredApps = computed(() => {
+      const search = searchQuery.value.toLowerCase();
+      return apps.value.filter(app => !searchQuery.value || app.name.toLowerCase().includes(search));
+    });
+
+    return {
+      searchQuery,
+      filteredApps,
+      openApp
+    };
+  }
+};
+</script>
 
 <style scoped>
 .app-grid {
